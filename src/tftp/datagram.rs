7b328fd547ae23cfc09b5cd42686ec31ee5c6356
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2022-07-28 14:34:50 +0200

    datagram: add tests

--- src/tftp/datagram.rs
+++ src/tftp/datagram.rs
@@ -135,3 +135,112 @@ impl <'a> Datagram<'a> {
 	}
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! assert_datagram {
+	($v:expr, $pat:pat) => {
+	    assert_datagram!($v, $pat, true)
+	};
+
+	($v:expr, $pat:pat, $test:expr) => {
+	    assert_datagram!($v, pat => Ok($pat), test => $test)
+	};
+
+	($v:expr, err => $pat:pat) => {
+	    assert_datagram!($v, pat => Err(Error::RequestError($pat)), test => true)
+	};
+
+	($v:expr, err => $pat:pat, $test:expr) => {
+	    assert_datagram!($v, pat => Err(Error::RequestError($pat)), test => $test)
+	};
+
+	($v:expr, pat => $pat:pat, test => $test:expr) => {
+	    match Datagram::try_from($v as &[u8]) {
+		$pat if $test		=> {},
+		Ok(v)			=> panic!("bad result {}", v),
+		Err(e)			=> panic!("err {:?}", e),
+	    }
+	};
+    }
+
+    #[test]
+    #[allow(unused_variables)]
+    fn test_datagram() {
+	use super::super::RequestError as RE;
+
+	// RRQ
+	assert_datagram!(b"\x00\x01file\x00octet\x00",    Datagram::Read(_rrq));
+	assert_datagram!(b"\x00\x01file\x00netascii\x00", Datagram::Read(_rrq));
+	assert_datagram!(b"\x00\x01file\x00binary\x00",   Datagram::Read(_rrq));
+	assert_datagram!(b"\x00\x01file\x00mail\x00",     Datagram::Read(_rrq));
+	assert_datagram!(b"\x00\x01file\x00mail\x00",     Datagram::Read(_rrq));
+
+	assert_datagram!(b"\x00\x01file\x00XXX\x00", err => RE::BadMode);
+	assert_datagram!(b"\x00\x01file\x00octet",   err => RE::MissingZero);
+	assert_datagram!(b"\x00\x01file\x00",        err => RE::MissingMode);
+	assert_datagram!(b"\x00\x01file",            err => RE::MissingZero);
+	assert_datagram!(b"\x00\x01",                err => RE::TooShort);
+
+	assert_datagram!(b"\x00\x01file\x00octet\x00\
+			   blksize\x002000\x00\
+			   unsupported\x001234\x00\
+			   timeout\x005\x00\
+			   tsize\x000\x00\
+			   windowsize\x0064\x00", Datagram::Read(rrq),
+			 rrq.get_filename().to_str().unwrap() == "file" &&
+			 rrq.mode == super::super::Mode::Octet &&
+			 rrq.block_size == Some(2000) &&
+			 rrq.timeout == Some(std::time::Duration::from_secs(5)) &&
+			 rrq.tsize == Some(0) &&
+			 rrq.window_size == Some(64));
+
+	assert_datagram!(b"\x00\x01file\x00octet\x00\
+			   tsize\x0042\x00", err => RE::NumberOutOfRange);
+
+	assert_datagram!(b"\x00\x01file\x00octet\x00\
+			   tsize\x0042\x00", err => RE::NumberOutOfRange);
+
+	assert_datagram!(b"\x00\x01file\x00octet\x00\
+			   windowsize\x000\x00", err => RE::NumberOutOfRange);
+
+	assert_datagram!(b"\x00\x01file\x00octet\x00\
+			   blksize\x007\x00", err => RE::NumberOutOfRange);
+
+	// WRQ
+	assert_datagram!(b"\x00\x02file\x00octet\x00",    Datagram::Write(_wrq));
+	assert_datagram!(b"\x00\x02file\x00octet\x00\
+			   tsize\x0042\x00", Datagram::Write(wrq),
+			 wrq.get_filename().to_str().unwrap() == "file" &&
+			 wrq.mode == super::super::Mode::Octet &&
+			 wrq.tsize == Some(42));
+
+	// DATA
+	assert_datagram!(b"\x00\x03\x01\x02data" as &[u8], Datagram::Data(seq, data),
+			 seq.as_u16() == 0x0102 && data == b"data");
+
+	// ACK
+	assert_datagram!(b"\x00\x04\x01\x02", Datagram::Ack(seq),
+			 seq.as_u16() == 0x0102);
+
+	assert_datagram!(b"\x00\x04\x01\x02X", err => RE::MalformedAck);
+
+	// ERROR
+	assert_datagram!(b"\x00\x05\x01\x02error\x00", Datagram::Error(code, msg),
+			 code == 0x0102 && msg == b"error");
+	assert_datagram!(b"\x00\x05\x01\x02\x00", Datagram::Error(code, msg),
+			 code == 0x0102 && msg == b"");
+
+	assert_datagram!(b"\x00\x05\x01\x02error", err => RE::MissingZero);
+
+	// OACK
+	assert_datagram!(b"\x00\x06...", Datagram::OAck);
+
+	// misc errors
+	assert_datagram!(b"\x00\x07", err => RE::BadOpCode(c), c == 7);
+	assert_datagram!(b"\x00",     err => RE::TooShort);
+	assert_datagram!(b"",         err => RE::TooShort);
+    }
+}
