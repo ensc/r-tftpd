8df2d0105bdc573b0d0593942363e3e159f93496
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2023-01-11 14:49:44 +0100

    sequence_id: remove non-power_of_two() mode

--- src/tftp/sequence_id.rs
+++ src/tftp/sequence_id.rs
@@ -1,5 +1,3 @@
-const SEQUENCE_SIZE: u32 = 65536;
-
 #[derive(Copy, Clone, Default, PartialEq, Eq)]
 pub struct SequenceId(u16);
 
@@ -21,20 +19,11 @@ impl SequenceId {
     }
 
     pub fn delta(self, other: Self) -> u16 {
-	match SEQUENCE_SIZE.is_power_of_two() {
-	    true	=> self.0.wrapping_sub(other.0),
-
-	    false if self.0 < other.0	=> u16::try_from(SEQUENCE_SIZE - (other.0 as u32) +
-							 (self.0 as u32)).unwrap(),
-	    false			=> self.0 - other.0
-	}
+	self.0.wrapping_sub(other.0)
     }
 
     pub fn add(self, other: Self) -> Self {
-	Self(match SEQUENCE_SIZE.is_power_of_two() {
-	    true	=> self.0.wrapping_add(other.0),
-	    false	=> (((self.0 as u32) + (other.0 as u32)) % SEQUENCE_SIZE) as u16,
-	})
+	Self(self.0.wrapping_add(other.0))
     }
 
     pub fn as_u16(self) -> u16 {
@@ -67,10 +56,7 @@ impl std::ops::AddAssign<u16> for SequenceId {
 #[cfg(test)]
 impl std::ops::SubAssign<u16> for SequenceId {
     fn sub_assign(&mut self, rhs: u16) {
-	self.0 = match SEQUENCE_SIZE.is_power_of_two() {
-	    true	=> self.0.wrapping_sub(rhs),
-	    false	=> ((SEQUENCE_SIZE + self.0 as u32 - rhs as u32) % SEQUENCE_SIZE) as u16,
-	}
+	self.0 = self.0.wrapping_sub(rhs);
     }
 }
 
@@ -78,10 +64,7 @@ impl std::ops::Add<u16> for SequenceId {
     type Output = Self;
 
     fn add(self, rhs: u16) -> Self::Output {
-	Self(match SEQUENCE_SIZE.is_power_of_two() {
-	    true	=> self.0.wrapping_add(rhs),
-	    false	=> (((self.0 as u32) + (rhs as u32)) % SEQUENCE_SIZE) as u16,
-	})
+	Self(self.0.wrapping_add(rhs))
     }
 }
 
