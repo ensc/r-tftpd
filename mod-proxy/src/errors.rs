a0b6263863bdd1250bbe0e93fc8b992be0c7de5c
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2025-04-23 15:43:47 +0200

    errors: minor optiomiziation
    use Box<str> instead of String to save 8 bytes

--- mod-proxy/src/errors.rs
+++ mod-proxy/src/errors.rs
@@ -13,7 +13,7 @@ pub enum Error {
 
     // helper because reqwest::Error does not implement Clone
     #[error("http error: {0}")]
-    HttpErrorStr(String),
+    HttpErrorStr(Box<str>),
 
     #[error("request failed with status {0}")]
     HttpStatus(reqwest::StatusCode),
@@ -35,7 +35,7 @@ impl Clone for Error {
         match self {
             Self::Io(e) => Self::Io(e.kind().into()),
             Self::Nix(arg0) => Self::Nix(*arg0),
-            Self::HttpError(arg0) => Self::HttpErrorStr(format!("{arg0}")),
+            Self::HttpError(arg0) => Self::HttpErrorStr(format!("{arg0}").into()),
             Self::HttpErrorStr(arg0) => Self::HttpErrorStr(arg0.clone()),
 	    Self::HttpStatus(s) => Self::HttpStatus(*s),
             Self::BadHttpTime => Self::BadHttpTime,
